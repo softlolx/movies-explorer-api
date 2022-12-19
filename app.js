5a772489c4919a4d78d11fd261bea8df51735ec7
softlolx <softlolx@gmail.com>
2022-12-19 19:17:11 +0300

    port change

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const limiter = require('./middlewares/limiter');
 const errorHandler = require('./middlewares/errorHandler');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
-const { PORT = 3000, MONGO_URL = 'mongodb://localhost:27017/moviesdb' } = process.env;
+const { PORT = 3002, MONGO_URL = 'mongodb://localhost:27017/moviesdb' } = process.env;
 
 const app = express();
 mongoose.connect(MONGO_URL);
